7703d535e139127755ac850d02e0d0b52d1aff27
jlaviole90 <jlaviole90@gmail.com>
2023-12-30 21:52:10 -0600

    init

---
+++ server/myshelf/src/test/java/com/myshelf/MyshelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
