d36e1f1fcede43bde8a1744b0286e8996959bc1e
jlaviole90 <jlaviole90@gmail.com>
2023-12-30 22:53:07 -0600

    update and update and update

---
+++ server/src/main/java/com/hominiv/controllers/InventoryController.java
@@ -0,0 +1,11 @@
+package com.hominiv.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InventoryController {
+
+    public InventoryController() {
+        return;
+    }
+}
